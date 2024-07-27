da25bd41555f6404bed8f3308914b2cfaaa38f00
ozgurd5 <ozgurdalbeler@gmail.com>
2024-07-28 00:34:31 +0300

    Prefab Update and ROcket Namespace

--- Assets/Scripts/Entities/Rocket.cs
+++ Assets/Scripts/Entities/Rocket.cs
@@ -1,36 +1,40 @@
-using UnityEngine;
 using DG.Tweening;
 using Sirenix.OdinInspector;
+using UnityEngine;
 
-public class Rocket : MonoBehaviour
+namespace Entities
 {
-    [SerializeField] private float shakeDuration = 1.0f;
-    [SerializeField] private float shakeStrength = 0.5f;
-    [SerializeField] private int shakeVibrato = 10;
-    [SerializeField] private float launchHeight = 50.0f;
-    [SerializeField] private float launchDuration = 5.0f;
-    [SerializeField] private float backDuration = 5.0f;
-    [SerializeField] private Ease launchEase = Ease.InOutQuad;
-
-    private Vector3 StartPosition => transform.position;
-    
-    
-
-    [Button]
-    private void Launch()
+    public class Rocket : MonoBehaviour
     {
-        transform.DOShakePosition(shakeDuration, shakeStrength, shakeVibrato).OnComplete(() =>
+        [Header("Parameters")] //todo: move to entity parameters
+        [SerializeField] private float shakeDuration = 1.0f;
+        [SerializeField] private float shakeStrength = 0.5f;
+        [SerializeField] private int shakeVibrato = 10;
+        [SerializeField] private float launchHeight = 50.0f;
+        [SerializeField] private float launchDuration = 5.0f;
+        [SerializeField] private float backDuration = 5.0f;
+        [SerializeField] private Ease launchEase = Ease.InOutQuad;
+
+        private Vector3 startPosition;
+
+        private void Start()
         {
+            startPosition = transform.position;
+        }
 
-            transform.DOLocalMoveY(launchHeight, launchDuration)
-                .SetEase(launchEase);
-        });
-    }
+        [Button]
+        private void Launch()
+        {
+            var sequence = DOTween.Sequence();
+            sequence.Append(transform.DOShakePosition(shakeDuration, shakeStrength, shakeVibrato));
+            sequence.Append(transform.DOLocalMoveY(launchHeight, launchDuration).SetEase(launchEase));
+            sequence.Play();
+        }
 
-    [Button]
-    public void BackToBase()
-    {
-        transform.DOLocalMoveY(StartPosition.y - 25, backDuration)
-            .SetEase(launchEase);
+        [Button]
+        public void BackToBase()
+        {
+            transform.DOLocalMoveY(startPosition.y, backDuration).SetEase(launchEase);
+        }
     }
 }
